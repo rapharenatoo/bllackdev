9019010f3e24edc7e6482d647731cd2ad59d7d3b
Raphael Renato <rapha_rmm@hotmail.com>
2023-10-28 12:57:15 -0300

    ✨ Create project details page

---
+++ app/components/back-to-top/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import { TbArrowNarrowUp } from "react-icons/tb";
+import { Button } from "../button";
+
+export const BackToTop = () => {
+  const [show, setShow] = useState(false);
+
+  const scrollToTop = () => {
+    console.log(">>> scrollToTop: ", window);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  const handleScroll = useCallback(() => {
+    if (!show && window.scrollY > 500) setShow(true);
+    if (show && window.scrollY <= 500) setShow(false);
+  }, [show]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    console.log(">>> scrollToTop: ", window);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
+
+  return (
+    <div className='fixed right-4 bottom-4 z-20'>
+      {show ? (
+        <Button
+          onClick={scrollToTop}
+          className='shadow-lg shadow-emerald-400/20'
+        >
+          <TbArrowNarrowUp size={20} />
+        </Button>
+      ) : null}
+    </div>
+  );
+};
--- app/components/button/index.tsx
+++ app/components/button/index.tsx
@@ -10,6 +10,7 @@ export const Button = ({ children, className, ...props }: ButtonProps) => {
         "bg-emerald-600 py-3 px-4 rounded-lg text-gray-50 flex items-center justufy-center gap-2 hover:bg-emerald-500 transition-all disabled:opacity-50",
         className
       )}
+      {...props}
     >
       {children}
     </button>
